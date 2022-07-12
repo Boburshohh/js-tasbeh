49849c559fd4f2b5a7481a07a9f8a354add00457
Boburshoh Khusanov <83607069+Boburshohh@users.noreply.github.com>
2022-07-12 17:07:54 +0500

    qoshildi

---
+++ yordamchi/js/app.js
@@ -0,0 +1,15 @@
+let tasbeh_clik = document.querySelector('.tasbeh_clik'),
+        btn = document.querySelector('btn'),
+        btns = document.getElementById('qwe')
+        zikr = document.querySelector('.zikr');
+        rang  = document.getElementById('rang')
+      
+    
+    // btn.addEventListener('click',  
+    function salom(){
+        // console.log(rang.value)
+        tasbeh_clik.style.backgroundColor = rang.value
+
+       
+    } 
+    
